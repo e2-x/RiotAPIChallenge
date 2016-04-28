79e2e98a22d29ec48055423ba07b7a0eba5de339
Edwin Tejeda <Edwin Tejeda>
2016-04-27 20:51:49 -0400

    Glyphicons add

--- js/main.js
+++ js/main.js
@@ -13,5 +13,23 @@ $(document).ready(function() {
 
           }
       });
+    });
+
+    $.ajax({
+    url:'https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=all&api_key=ea3e1ffd-a2f4-41a5-ae49-4b5d1ef6f960',
+    type:'GET',
+    dataType: 'json',
+    data: {},
+    success: function(json) {
+      console.log(json);
+    }
+
+    });
   });
-});
+
+
+function champLookUp() {
+  var champName = "";
+  champName = $("#cId").val();
+  champName = champName.toLowerCase();
+}
